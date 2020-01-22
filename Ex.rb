b9bcb962c77d4a86818ab478091c06a98df84821
shubham-420 <smehta@grepruby.com>
2020-01-22 18:51:19 +0530

    Problem.rb

---
+++ Ex.rb
@@ -0,0 +1,92 @@
+a ="hi dfhi hids hisd hi dsfhi"
+# puts "#{a.slice()}"
+puts a[0..-7]
+# puts "#{a[2,4]}"
+# puts "#{a.slice(1..4)}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sl=0
+# tax=0.00
+# k=0
+# sltax=0.00
+# arr=[]
+# for i in (0..3)	
+
+# 	p=0.00
+# 	st=0
+# 	temp=0
+# 	s=gets.chomp
+# 	word=s.split
+# 	n=word.length
+# 	price=word[n-1]
+# 	imp=0
+
+# 	word.each{|n| st=1  if n == "book" || n == "chocolate" || n == "pills" }
+# 	word.each{|n| imp=1  if n == "imported" }
+# 	if imp == 1
+# 		tax=5.00	
+# 	else
+# 		tax=0.00
+# 	end
+
+# 	if st==1
+# 		var = tax/100.to_f
+# 		sltax = sltax.to_f+( word[n-1].to_f * var.to_f ).to_f
+# 		t = ( ( word[n-1].to_f * var.to_f ) + word[n-1].to_f )
+# 		# t = word[n-1].to_f
+# 	else
+# 		tax=tax.to_f+10.00
+# 		var = tax/100.to_f
+# 		sltax=sltax.to_f+ ( word[n-1].to_f * var.to_f ).to_f
+# 		t = ( ( word[n-1].to_f * var.to_f ) + word[n-1].to_f )
+		 
+
+		# t = (word[n-1].to_f * st.to_f) + word[n-1].to_f
+		
+# 	end
+
+
+# 	arr[k]=t
+# 	k=k+1
+
+
+
+# end
+
+
+# puts arr
+# puts sltax
+# puts arr.sum.to_f
+
+# puts word[n-1]
+
+# p=0.00
+# s=gets
+# word=s.split
+# n=word.length
+# for "book" in word
+# 	if i=="book" ||i=="pills" || i=="chocolate"
+# 		temp=1
+# 		break
+# 	else
+# 		temp=0
+# 	end
+# end
+
+
