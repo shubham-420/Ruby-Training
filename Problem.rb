43900869637561d345f81eba400945d35d658f82
shubham-420 <smehta@grepruby.com>
2020-01-23 18:57:32 +0530

    sales_texa_calculator

--- Problem.rb
+++ Problem.rb
@@ -1,66 +1,72 @@
-class Problem
-	def fun 
-			sl = 0
-			tax = 0.00
-			k = 0
-			sentence = []
-			sltax = 0.00
-			ss = 0
-			arr = []
-			for i in (0..2)	
-				p = 0.00
-				st = 0
-				temp = 0
-				s = gets.chomp
-				temp1 = s[0..-9]
-				word = s.split
-				n = word.length
-				price = word[n-1]
-				imp = 0
+class Sales_Tax_Problem
 
-				sentence[ss] = temp1
-				ss = ss + 1
-				word.each{|n| st=1  if n == "book" || n == "chocolate" || n == "pills" }
-				word.each{|n| imp=1  if n == "imported" }
-				if imp == 1
-					tax = 5.00	
-				else
-					tax = 0.00
-				end
+  def calucate_tax(tax, price)
+    total_amount = tax/100.to_f
+    @salestax = @salestax.to_f + ( price.to_f * total_amount )
+    ( ( price.to_f * total_amount ) + price )
+  end
 
-				if st == 1
-					var = tax/100.to_f
-					sltax = sltax.to_f + ( word[n-1].to_f * var.to_f ).to_f
-					t = ( ( word[n-1].to_f * var.to_f ) + word[n-1].to_f )
-					# t = word[n-1].to_f
-				else
-					tax = tax.to_f + 10.00
-					var = tax/100.to_f
-					sltax = sltax.to_f+ ( word[n-1].to_f * var.to_f ).to_f
-					t = ( ( word[n-1].to_f * var.to_f ) + word[n-1].to_f )
-					 
+  def total_tax(tax,price)
+    tax = tax.to_f + 10.00
+    total_amount = tax/100.to_f
+    @salestax = @salestax.to_f+ ( price.to_f * total_amount.to_f )
+    ( price.to_f * total_amount.to_f ) + price.to_f
+  end
 
-					# t = (word[n-1].to_f * st.to_f) + word[n-1].to_f
-					
-				end
-				arr[k] = t
-				k = k + 1
-			end
+  def item_exempted(input)
+    flag = false
+    ["book","chocolate","pills"].each do |element|
+    if input.include?(element)
+      flag=true
+    end
+    
+    flag
+  end
 
-		
-			puts "#{sentence[0]} : #{arr[0]}"
-			puts "#{sentence[1]} : #{ arr[1]}"
-			puts "#{sentence[2]} : #{arr[2]}" 
-			d = 0.00
-		  d = arr.sum.to_f
-      d = d.round(2)
-		  sltax = sltax.round(2) 
-	  	puts "Sales taxes : #{sltax}"
-		  puts "Total #{d}"
+  def item_imported?(input)
+    input.include?("imported")
+  end
 
-	end
+  def display(sentence,total_cost)
+    puts " output is: "
+    
+    for index in (0..2)
+      puts "#{sentence[index]} : #{total_cost[index]}"
+    end
+    
+    sum = total_cost.sum.to_f
+    sum = sum.round(2)
+    @salestax = @salestax.round(2)
+    
+    puts "Sales taxes : #{@salestax}"
+    puts "Total #{sum}"
+  end
+
+  def tax_calculator 
+    tax, sentence, @salestax, total_cost  = 0.00, [], 0.00,  []
+
+    for iterate in (0..3) 
+      input = gets.chomp
+      output = input.split("at")
+      price = output[1].to_f
+      sentence[iterate] = output[0]
+
+      tax = item_imported(input) ? 5.00 : 0.00
+
+      if item_exempted(input)
+        after_tax_cost=calucate_tax(tax,price)
+      else
+        after_tax_cost=total_tax(tax,price)
+      end
+
+      total_cost[iterate] = after_tax_cost
+    end
+
+    display(sentence,total_cost)
+
+  end
+
 end
 
-obj=Problem.new 
-obj.fun
-
+run = Sales_Tax_Problem.new
+run.tax_calculator
